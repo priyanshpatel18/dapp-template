9c71547122b20fb04d2f56561915b6e98fae7be4
priyanshpatel18 <priyansh.180804@gmail.com>
2025-02-28 01:10:47 +0530

    added wallet connection safety for proper crud usage

--- src/app/crud/page.tsx
+++ src/app/crud/page.tsx
@@ -1,4 +1,5 @@
 import { CrudInitialize } from "@/components/crud/crud-ui";
+import { WalletButton } from "@/components/WalletButton";
 import { montserrat } from "@/lib/fonts";
 
 export default function page() {
@@ -6,6 +7,7 @@ export default function page() {
     <div className="h-screen max-w-3xl m-auto py-10 flex flex-col items-center gap-4">
       <h1 className={`text-3xl text-center font-semibold ${montserrat.className}`}>Use CRUD program</h1>
 
+      <WalletButton />
       <CrudInitialize />
     </div>
   )
--- src/components/crud/crud-ui.tsx
+++ src/components/crud/crud-ui.tsx
@@ -1,14 +1,26 @@
 "use client";
 
+import { useWallet } from "@solana/wallet-adapter-react";
+import { Keypair } from "@solana/web3.js";
+import { toast } from "sonner";
 import { Button } from "../ui/button";
 import { useCrudProgram } from "./crud-data-access";
-import { Keypair } from "@solana/web3.js"
 
 export function CrudInitialize() {
   const { initialize } = useCrudProgram()
+  const { publicKey } = useWallet()
+
+  if (!publicKey) {
+    return <p>Wallet not connected</p>
+  }
 
   return (
-    <Button onClick={() => initialize.mutateAsync(Keypair.generate())}>
+    <Button onClick={() => {
+      if (!publicKey) {
+        return toast.error('Wallet not connected');
+      }
+      initialize.mutateAsync(Keypair.generate())
+    }}>
       Initialize
     </Button>
   );
